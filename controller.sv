cf2408fa89a59a88de7838aa90068a11aa29a75e
louay-hesham <louay.hesham@gmail.com>
2017-04-21 00:36:56 +0200

    changes for BNE

--- controller.sv
+++ controller.sv
@@ -3,12 +3,12 @@ module controller(	input logic [5:0] op, funct,
 			output logic memtoreg, memwrite,
 			output logic pcsrc, alusrc,
 			output logic regdst, regwrite, 
-			output logic jump, 
+			output logic jump, bne,
 			output logic [2:0] alucontrol);
 
 	logic [1:0] aluop;
 	logic branch;
-	maindec md(op, memtoreg, memwrite, branch, alusrc, regdst, regwrite, jump, aluop);
+	maindec md(op, memtoreg, memwrite, branch, alusrc, regdst, regwrite, jump, bne, aluop);
 	aludec ad(funct, aluop, alucontrol);
-	assign pcsrc=branch & zero;
+	assign pcsrc = (branch & zero) | (bne & ~zero);
 endmodule
